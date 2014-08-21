273d70bdf11e45e6e6114646d9878591fe36cec4
Jean-Charles Sisk <jasisk@gmail.com>
2014-08-20 21:47:36 -0400

    running pre template in separate ctx

--- helpers/compiled-templates.js
+++ helpers/compiled-templates.js
@@ -1,9 +1,11 @@
-var dust = module.exports = require('dustjs-linkedin');
+'use strict';
 
-(function(){dust.register("block-pre",body_0);var blocks={"bodyContent":body_1};function body_0(chk,ctx){ctx=ctx.shiftBlocks(blocks);return chk.partial("layout-pre",ctx,{});}function body_1(chk,ctx){ctx=ctx.shiftBlocks(blocks);return chk.write("<p>I'm a different ").reference(ctx.get(["whatAmI"], false),ctx,"h").write(" template.</p>");}return body_0;})();
+var dust = require('dustjs-linkedin');
+var path = require('path');
+var vm = require('vm');
+var fs = require('fs');
 
-(function(){dust.register("layout-pre",body_0);function body_0(chk,ctx){return chk.write("<!DOCTYPE html><html><head><title>Check me out.</title></head><body>").block(ctx.getBlock("bodyContent"),ctx,{},{}).write("</body></html>");}return body_0;})();
+var source = fs.readFileSync(path.join(__dirname, 'templates.precompiled'));
+var ctx = vm.createContext({ dust: dust });
 
-(function(){dust.register("gist-layout-pre",body_0);function body_0(chk,ctx){return chk.write("<!DOCTYPE html><html xmlns=\"http://www.w3.org/1999/xhtml\"><head><title>Dust.js Test Template</title></head><body>").block(ctx.getBlock("body"),ctx,{},{}).write("</body></html>");}return body_0;})();
-
-(function(){dust.register("gist-block-pre",body_0);var blocks={"body":body_1};function body_0(chk,ctx){ctx=ctx.shiftBlocks(blocks);return chk.partial("gist-layout-pre",ctx,{});}function body_1(chk,ctx){ctx=ctx.shiftBlocks(blocks);return chk.write("<h1>REAL DUSTY</h1><ul>").section(ctx.get(["TopNavigation"], false),ctx,{"block":body_2},{}).write("</ul>").section(ctx.get(["Products"], false),ctx,{"block":body_3},{});}function body_2(chk,ctx){ctx=ctx.shiftBlocks(blocks);return chk.write("<li>").reference(ctx.get(["Title"], false),ctx,"h").write("</li>");}function body_3(chk,ctx){ctx=ctx.shiftBlocks(blocks);return chk.reference(ctx.get(["Name"], false),ctx,"h");}return body_0;})();
+vm.runInContext(source, ctx, 'templates.vm');
